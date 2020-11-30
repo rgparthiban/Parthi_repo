fa36be6374d0f7486ee74bff6e4143bd86db5eae
rgparthiban <rgparthiban@gmail.com>
2020-11-30 18:19:28 +0530

    commiting

---
+++ .sfdx/typings/lwc/siteAsset_cff87f6665ab46a88c420324e0acb30d.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/siteAsset_cff87f6665ab46a88c420324e0acb30d" {
+    var siteAsset_cff87f6665ab46a88c420324e0acb30d: string;
+    export default siteAsset_cff87f6665ab46a88c420324e0acb30d;
+}
---
+++ .sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,32 @@
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/countup7" {
+    var countup7: string;
+    export default countup7;
+}
+declare module "@salesforce/resourceUrl/heatmap" {
+    var heatmap: string;
+    export default heatmap;
+}
+declare module "@salesforce/resourceUrl/heatmapmock" {
+    var heatmapmock: string;
+    export default heatmapmock;
+}
+declare module "@salesforce/resourceUrl/leaflet" {
+    var leaflet: string;
+    export default leaflet;
+}
+declare module "@salesforce/resourceUrl/leaflet1" {
+    var leaflet1: string;
+    export default leaflet1;
+}
+declare module "@salesforce/resourceUrl/nouislider" {
+    var nouislider: string;
+    export default nouislider;
+}
+declare module "@salesforce/resourceUrl/siteAsset_cff87f6665ab46a88c420324e0acb30d" {
+    var siteAsset_cff87f6665ab46a88c420324e0acb30d: string;
+    export default siteAsset_cff87f6665ab46a88c420324e0acb30d;
+}
